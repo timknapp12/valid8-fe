31b7a2580ef9c2991a9e20bb07a30756c2392a5d
Tim Knapp <timknapp12@gmail.com>
2024-10-21 02:58:26 -0600

    add global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: { colors: { white: '#ffffff', black: '#121212' } },
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+        darkGray: '#333333',
+        lightGray: '#f5f5f5',
+        blue: '#007bff',
+      },
+    },
   },
   plugins: [],
 };

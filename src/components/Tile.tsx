6d3f97a13e2071f8b7811723798414acf2bf8f89
Tim Knapp <timknapp12@gmail.com>
2024-10-21 03:24:35 -0600

    give tile exact dimensions

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,16 +3,18 @@ import { TileProps } from '../types';
 
 export const Tile: React.FC<TileProps> = ({ name, owner, link }) => {
   return (
-    <div className='bg-white rounded-lg shadow-md p-4 m-4 transition-shadow hover:shadow-lg max-w-xs w-full'>
-      <h3 className='text-lg font-semibold mb-2 text-gray-800 break-words'>
-        {name}
-      </h3>
-      <p className='text-sm text-gray-600 break-words'>{owner}</p>
+    <div className='bg-white rounded-lg shadow-md p-4 m-4 transition-shadow hover:shadow-lg w-64 h-48 flex flex-col justify-between'>
+      <div>
+        <h3 className='text-lg font-semibold mb-2 text-gray-800 break-words'>
+          {name}
+        </h3>
+        <p className='text-sm text-gray-600 break-words'>{owner}</p>
+      </div>
       <a
         href={link}
         target='_blank'
         rel='noreferrer'
-        className='text-blue hover:underline truncate block'
+        className='text-blue hover:underline truncate block mt-2'
       >
         {link}
       </a>

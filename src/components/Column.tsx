c093bf9bc3afba3c7287f3668fa7cfe8b9adf0a4
Tim Knapp <timknapp12@gmail.com>
2024-10-21 03:24:18 -0600

    refine container styles

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { ColumnProps } from '../types';
 
-export const Column: React.FC<ColumnProps> = ({ className = '', children }) => {
+export const Column: React.FC<ColumnProps> = ({
+  className = '',
+  children,
+  style,
+}) => {
   return (
     <div
       className={`flex flex-col w-full gap-4 items-center justify-start p-4 relative ${className}`}
+      style={style}
     >
       {children}
     </div>
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -4,7 +4,7 @@ import { RowProps } from '../types';
 export const Row: React.FC<RowProps> = ({ className = '', children }) => {
   return (
     <div
-      className={`flex items-center justify-between p-4 relative ${className}`}
+      className={`flex items-center gap-4 justify-between p-4 relative ${className}`}
     >
       {children}
     </div>

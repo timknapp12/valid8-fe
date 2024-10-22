78e5eb0ceec78a5bd5a1094a56f120c2ff4a6c26
Tim Knapp <timknapp12@gmail.com>
2024-10-21 23:47:43 -0600

    expand mock items

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,39 +1,6 @@
 import React, { createContext, useContext, ReactNode, useState } from 'react';
 import { AppContextType } from '../types';
-
-const mockItems = [
-  {
-    id: 1,
-    name: 'example-repo-name',
-    owner: 'deanmlittle',
-    link: 'https://google.com',
-  },
-  {
-    id: 2,
-    name: 'a-little-bit-longer-repo-name',
-    owner: 'reallyReallyReallyLongOwnerName',
-    link: 'https://really/long/link/google.com/long/name/kljlkdjfkl/realllllylongname',
-  },
-  {
-    id: 3,
-    name: 'vault-2024',
-    owner: 'ASCorreia',
-    link: 'https://github.com/ASCorreia/vault-2024',
-  },
-  { id: 4, name: 'example4', owner: 'sallysmith', link: 'https://google.com' },
-  {
-    id: 5,
-    name: 'really-really-extra-long-super-duper-long-repo-name',
-    owner: 'jimmyanderson',
-    link: 'https://google.com',
-  },
-  {
-    id: 6,
-    name: 'example6',
-    owner: 'jillanderson',
-    link: 'https://google.com/kljlkdjfkl/realllllylongname',
-  },
-];
+import { mockItems } from './mockItems';
 
 const AppContext = createContext<AppContextType>({} as AppContextType);
 export const useAppContext = () => useContext(AppContext);

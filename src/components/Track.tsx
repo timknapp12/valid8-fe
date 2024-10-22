ab40828139cfb2ad75b6c4db77b0bd32d7410fb0
Tim Knapp <timknapp12@gmail.com>
2024-10-21 23:48:28 -0600

    fine tune styles for track

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -6,20 +6,29 @@ import { TrackProps } from '../types';
 export const Track: React.FC<TrackProps> = ({ items }) => {
   return (
     <div className='w-full overflow-x-auto bg-white bg-opacity-20 h-[260px] flex flex-col justify-center items-start relative'>
-      <div className='flex-grow flex items-center'>
+      <div className='w-full flex items-center'>
         {items.length === 0 ? (
-          <Row style={{ justifyContent: 'center', width: '100%' }}>
+          <Row
+            className='w-full'
+            style={{
+              justifyContent: 'center',
+              width: '100%',
+            }}
+          >
             <p className='text-white'>No results found</p>
           </Row>
         ) : (
           <Row
             className='flex-nowrap relative'
-            style={{ justifyContent: 'flex-start' }}
+            style={{
+              justifyContent: 'flex-start',
+              gap: '2rem',
+              padding: '0 2rem',
+            }}
           >
             {items.map(({ id, name, owner, link }) => (
               <Tile key={id} id={id} name={name} owner={owner} link={link} />
             ))}
-            <div className='absolute right-0 top-0 bottom-0 w-8 pointer-events-none'></div>
           </Row>
         )}
       </div>

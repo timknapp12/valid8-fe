fe8d76becbb88cc8de19e9ec33b4c1898caed14e
Tim Knapp <timknapp12@gmail.com>
2024-10-23 04:28:26 -0600

    add secondary and disabled prop to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,23 +4,25 @@ import { ButtonProps } from '../types';
 export const Button = ({
   onClick,
   loading,
+  disabled,
   children,
   className = '',
+  secondary = false,
 }: ButtonProps) => {
+  const baseStyles =
+    'relative px-4 py-2 font-bold rounded-md disabled:opacity-70 disabled:cursor-not-allowed min-w-36 flex flex-row items-center justify-center min-h-[40px]';
+
+  const variantStyles = secondary
+    ? 'text-darkGray border border-darkGray hover:border-hotPink hover:text-hotPink'
+    : 'bg-hotPink text-black hover:bg-darkHotPink';
+
   return (
     <button
       onClick={onClick}
-      disabled={loading}
-      className={`relative px-4 py-2 bg-hotPink text-black font-bold rounded-md 
-        disabled:opacity-70 disabled:cursor-not-allowed hover:bg-darkHotPink min-w-36 ${className}`}
+      disabled={loading || disabled}
+      className={`${baseStyles} ${variantStyles} ${className}`}
     >
-      {loading ? (
-        <>
-          <FaSpinner className='inline-block animate-spin' />
-        </>
-      ) : (
-        children
-      )}
+      {loading ? <FaSpinner className='inline-block animate-spin' /> : children}
     </button>
   );
 };
